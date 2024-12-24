b8984980d6766e91df668daae7b02b5cc8dcc768
Urjeet Patel <violentmagician.opensource@gmail.com>
2024-12-24 16:55:52 -0600

    feat(Frontend): Add Navbar and Footer components, update layout structure, and enhance Nginx configuration

---
+++ frontend/src/lib/components/layout/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="font-sans tracking-wide bg-[#213343] pt-2 pb-2 px-5">
+    <div class="border-t text-center border-[#6b5f5f] ">
+      <p class="text-gray-300 text-[15px]">
+        © ReadymadeUI. All rights reserved.
+      </p>
+    </div>
+  </footer>
+<!-- https://readymadeui.com/tailwind-components/footer -->
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import Navbar from '$lib/components/layout/Navbar.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 	import '../app.css';
 	let { children } = $props();
-</script>
 
-{@render children()}
+</script>
+<div class="flex flex-col h-screen">
+	<Navbar />
+	<main class="flex-1">
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
